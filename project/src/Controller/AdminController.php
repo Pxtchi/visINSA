ac240009e9e3ac62c3a7d8b5a75a1e4d892e2754
DragonKEME <kiki.lassa@outlook.com>
2023-03-09 11:29:39 +0100

    Add : dropdown bar

--- project/src/Controller/AdminController.php
+++ project/src/Controller/AdminController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Utilisateur;
-use App\Repository\UtilisateurRepository;
+use App\Entity\Roles;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,8 +14,10 @@ class AdminController extends AbstractController{
     public function panel(EntityManagerInterface $doctrine): Response
     {
         $users = $doctrine->getRepository(Utilisateur::class)->getJoueurs();
+        $roles = new Roles();
         return $this->render('admin/adminPanel.html.twig', [
-            'all_users' => $users
+            'all_users' => $users,
+            'roles' => $roles->getAllRoles()
         ]);
     }
 }
--- project/src/Entity/Roles.php
+++ project/src/Entity/Roles.php
@@ -1,6 +1,13 @@
 <?php
+
+namespace App\Entity;
+
 class Roles{
     const ROLE_ADMIN = 'ROLE_ADMIN';
     const ROLE_CONCEPTEUR = 'ROLE_CONCEPTEUR';
     const ROLE_USER = 'ROLE_USER';
+
+    public function getAllRoles() : array {
+        return [self::ROLE_ADMIN,self::ROLE_CONCEPTEUR,self::ROLE_USER];
+    }
 }
