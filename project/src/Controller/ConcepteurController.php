2b746760f55d5377fd7635c3f35dca470031938d
Alice Vancypher <alicevancypher@gmail.com>
2023-03-23 07:29:04 +0100

    début concepteur

--- project/src/Controller/ConcepteurController.php
+++ project/src/Controller/ConcepteurController.php
@@ -1,25 +1,49 @@
 <?php
 namespace App\Controller;
 
-use App\Repository\AventureRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Aventure;
 
 class ConcepteurController extends AbstractController
 {
-    #[Route('/concepteur/1', name: 'concepteur_frame1')]
-    public function frame1(AventureRepository $aventures): Response
+    #[Route('/concepteur', name: 'concepteur')]
+    public function concepteur(ManagerRegistry $doctrine): Response
     {
-        $aventures = $aventures->findAll();
-        return $this->render('concepteur/frame1.html.twig', [
+        $aventures = $doctrine->getRepository(Aventure::class)->findAll();
+        return $this->render('concepteur/concepteur.html.twig', [
             'aventures' => $aventures
         ]);
     }
 
-    #[Route('/concepteur/2', name: 'concepteur_frame2')]
-    public function concepteurHome(): Response
+    #[Route('/concepteur/modif_aventure/{nomaventure}', name: 'modif_aventure')]
+    public function modif_aventure(EntityManagerInterface $entityManager, string $nomaventure): Response
     {
-        return $this->render('concepteur/frame2.html.twig');
+        $repository = $entityManager->getRepository(Aventure::class);
+        $aventure = $repository->find($nomaventure);
+
+        $form = $this->createFormBuilder($aventure)
+            ->add('nomaventure', EntityType::class, [
+                'class' => Aventure::class,
+            ])
+            ->add('save', SubmitType::class, ['label' => 'Create Task'])
+            ->getForm();
+
+        return $this->render('concepteur/modif_aventure.html.twig', [
+            'aventure' => $aventure,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/navbar', name: 'navbar')]
+    public function navbar(): Response
+    {
+        return $this->render('concepteur/navbar.html.twig');
     }
 }
