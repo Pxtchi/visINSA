b404306bb8c20f96120e43f1cc35785bb5cc2e56
Alice Vancypher <alicevancypher@gmail.com>
2023-03-30 08:23:41 +0200

    accueil et modif_aventure

--- project/src/Controller/ConcepteurController.php
+++ project/src/Controller/ConcepteurController.php
@@ -10,6 +10,9 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Doctrine\Persistence\ManagerRegistry;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Aventure;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\SearchType;
+use App\Services\QrcodeService;
 
 class ConcepteurController extends AbstractController
 {
@@ -18,12 +21,23 @@ class ConcepteurController extends AbstractController
     {
         $aventures = $doctrine->getRepository(Aventure::class)->findAll();
         return $this->render('concepteur/concepteur.html.twig', [
-            'aventures' => $aventures
+            'aventures' => $aventures,
+            'titre' => 'Concepteur',
         ]);
     }
 
     #[Route('/concepteur/modif_aventure/{nomaventure}', name: 'modif_aventure')]
     public function modif_aventure(EntityManagerInterface $entityManager, string $nomaventure): Response
+    {
+        $repository = $entityManager->getRepository(Aventure::class);
+        $aventure = $repository->find($nomaventure);
+
+        return $this->render('concepteur/modif_aventure.html.twig', [
+            'aventure' => $aventure,
+        ]);
+    }
+
+    public function modif_question(EntityManagerInterface $entityManager, string $nomaventure): Response
     {
         $repository = $entityManager->getRepository(Aventure::class);
         $aventure = $repository->find($nomaventure);
@@ -35,15 +49,53 @@ class ConcepteurController extends AbstractController
             ->add('save', SubmitType::class, ['label' => 'Create Task'])
             ->getForm();
 
-        return $this->render('concepteur/modif_aventure.html.twig', [
+        return $this->render('concepteur/modif_question.html.twig', [
             'aventure' => $aventure,
             'form' => $form,
         ]);
     }
 
-    #[Route('/navbar', name: 'navbar')]
-    public function navbar(): Response
+    public function modif_etape(EntityManagerInterface $entityManager, string $nomaventure): Response
     {
-        return $this->render('concepteur/navbar.html.twig');
+        $repository = $entityManager->getRepository(Aventure::class);
+        $aventure = $repository->find($nomaventure);
+
+        $form = $this->createFormBuilder($aventure)
+            ->add('nomaventure', EntityType::class, [
+                'class' => Aventure::class,
+            ])
+            ->add('save', SubmitType::class, ['label' => 'Create Task'])
+            ->getForm();
+
+        return $this->render('concepteur/modif_etape.html.twig', [
+            'aventure' => $aventure,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/qrcodegenerator', name: 'qrcodegenerator')]
+    /**
+     * @param Request $request
+     * @param QrcodeService $qrcodeService
+     * @return Response
+     */
+    public function qrcodegenerator(Request $request, QrcodeService $qrcodeService): Response
+    {
+        #composer require endroid/qr-code
+        #composer require endroid/qr-code-bundle
+        #enable php-gd
+        $qrCode = null;
+        $form = $this->createForm(SearchType::class, null);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $qrCode = $qrcodeService->qrcode($data['name']);
+        }
+
+        return $this->render('concepteur/default/index.html.twig', [
+            'form' => $form->createView(),
+            'qrCode' => $qrCode
+        ]);
     }
 }
